b5713576c1053c7b35ac7d6156726cd03871b46f
MDDP <mael.debs@gmail.com>
2016-04-17 17:41:42 +0200

    Update ajouterArticle.php

--- ajouterArticle.php
+++ ajouterArticle.php
@@ -1,11 +1,16 @@
 <?php
+	include_once("recherche.php")
 	function ajouterArticle ($title, $content) {
-		$req = "INSERT INTO article user, title, content, date VALUES \"$_SESSION["user"]\", \"$title\", \"$content\", NOW()";
-		$con = mysql_connect($serv, $user);
-		if (!cherche($titre, "Titre", $con) {
+		$server="pams.script.univ-paris-diderot.fr";
+		$user="phiear22";
+		$base="phiear22";
+		$connexion=mysql_connect($server,$user,'r1M)qu0K');
+		mysql_select_db($base,$connexion);
+		$req = "INSERT INTO article user, title, content, date VALUES \"$_SESSION[\"user\"]\", \"$title\", \"$content\", NOW()";
+		if (!cherche($titre, "Titre", $connexion) {
 			return "Ce titre existe déjà";
 		} else {
-			$result = mysql_query($con, $req);
+			$result = mysql_query($connexion, $req);
 			if (!$result) {
 				return "Impossible d'accéder à votre requête.";
 			} else {
