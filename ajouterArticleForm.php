d6b0a979543dd45dff044cd3b7e9e8e4a202b1ef
MDDP <mael.debs@gmail.com>
2016-05-05 17:45:52 +0200

    Update ajouterArticleForm.php

--- ajouterArticleForm.php
+++ ajouterArticleForm.php
@@ -7,19 +7,19 @@
 	<div>
 		<form method="post" action="ajouterArticle.php">
 			<?php
-				if(isset($_POST["title"]{
-					$value = $_POST["title"];
-				} else {
-					$value = "Votre titre";
-				}
-				echo("<label> Titre : <label> <input type=\"text\" name=\"title\" size=\"50\" value=$value maxlength=\"200\" required>");
+			if(isset($_POST["title"]{
+				$value = $_POST["title"];
+			} else {
+				$value = "Votre titre";
+			}
+			echo("<label> Titre : <label> <input type=\"text\" name=\"title\" size=\"50\" value=$value maxlength=\"200\" required>");
 
-				if(isset($_POST["content"])){
-					$value = $_POST["content"];
-				} else {
-					$value = "Votre article";
-				}
-				echo("<textarea name=\"content\" rows=\"50\" cols=\"50\" value=$value maxlength=\"20000\" required> Ecrivez votre article ici </textarea>");
+			if(isset($_POST["content"])){
+				$value = $_POST["content"];
+			} else {
+				$value = "Votre article";
+			}
+			echo("<textarea name=\"content\" rows=\"50\" cols=\"50\" value=$value maxlength=\"20000\" required> Ecrivez votre article ici </textarea>");
 			?>
 			<input type="submit" value="Envoyer">
 		</form>
