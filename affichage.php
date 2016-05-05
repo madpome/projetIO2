1782db503430ce31d4dfb74f63e9afb488c53ee7
MDDP <mael.debs@gmail.com>
2016-05-05 17:40:17 +0200

    Update affichage.php

--- affichage.php
+++ affichage.php
@@ -1,9 +1,11 @@
 <?php
+	//Permet l'affichage d'un article entier
 	function  affiche_article($article) {
 		echo("<div><label> $article[\"title\"] </label> <label> par $article[\"user\"] </label> <label> écrit le $article[\"date\"]</div>");
 		echo("<div> $article[\"content\"] </div>");
 	}
 	
+	//Permet l'affichage partiel d'un article dans la recherche et la page d'accueil
 	function affichage_partiel($article) {
 		echo("<div><label> $article[\"title\"] </label> <label> par $article[\"user\"] </label>");
 		echo("<img src= ");
