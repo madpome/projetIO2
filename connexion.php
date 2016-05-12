2443469a7e13038dce0fe7e81965b03cb868a2b0
Philippe <earphilippefr@gmail.com>
2016-05-12 14:31:03 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -11,7 +11,7 @@ function connexion($login,$mdp){
 	$ligne=mysql_fetch_assoc($resultat);
 	if ($ligne && $mdp==password_verify($mdp,$ligne["pwd"])){
 		$_SESSION["login"]=$login;
-		$_SESSION["mail"]=$_ligne["mail"];
+		$_SESSION["mail"]=$ligne["mail"];
 		return true;
 	}else{
 		return false;
@@ -20,10 +20,18 @@ function connexion($login,$mdp){
 }
 //Met les attributs utiles de l'utilisateur dans la session
 function getatt(){
-	$_SESSION["user"]=$_POST["user"];
-	$_SESSION["mail"]=$_POST["mail"];
-	$_SESSION["lastname"]=$_POST["lastname"];
-	$_SESSION["firstname"]=$_POST["firstname"];
+	$server="pams.script.univ-paris-diderot.fr";
+	$user="phiear22";
+	$base="phiear22";
+	$connexion=mysql_connect($server,$user,'r1M)qu0K');
+	mysql_select_db($base,$connexion);
+	$req='SELECT user,mail,lastname,firstname,rank FROM users WHERE login='.$_SESSION["login"];
+	$result=mysql_query($req);
+	$attribut=mysql_fetch_assoc($result);
+	$_SESSION["user"]=$attribut["user"];
+	$_SESSION["mail"]=$attribut["mail"];
+	$_SESSION["lastname"]=$attribut["lastname"];
+	$_SESSION["firstname"]=$attribut["firstname"];
 	$_SESSION["connect"]=true;
 }
 //Set la valeur connecté pour la premiere connexion et permet de savoir si on est connecté ou pas
