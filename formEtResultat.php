9a59daafbfc8b8809a1f249f32dfd5113a5b2502
Philippe <earphilippefr@gmail.com>
2016-05-12 23:10:20 +0200

    Update formEtResultat.php

--- formEtResultat.php
+++ formEtResultat.php
@@ -1,52 +1,43 @@
 <?php
-	function jsapais(){
+	function formrechercherarticle(){
 ?>
 	<div>
 		<form action="resultat.php" method="post">
-			<label> Auteur : </label><input type="text" name="auteur">
-			<label> Date : </label><input type="text" name="date"> 
-			<label> Titre : </label><input type="text" name="title">
-			<label> Rechercher dans (catégorie?) </label><select name="category">
+			 Auteur :<input type="text" name="auteur"><br>
+			 Date :<input type="date" name="date"> <br>
+			 Titre :<input type="text" name="title"><br>
+			 Rechercher dans (catégorie?):
+			 <select name="category">
 					<option value="cinema"> Cinéma </option>
 					<option value="litterature"> Littérature </option>
 					<option value="autre">	Autre </option>
-					<option value="noValue" selected> Toutes </option>
+					<option value="all" selected> Toutes </option>
 					<!-- Insérer ici de nouvelles catégories-->
 				</select>
 			<input type="submit" value="Lancer la recherche">
 		</form>
 	</div>
-	
-	<div>
-		<?php
+<?php
+	}
+	function (){
 		$données;
 		$attr;
-		
 		if (isset($_POST["auteur"])) {
-			$données[0] = htmlspecialchars($_POST["auteur"]);
+			$données[0] =$_POST["auteur"]);
 			$attr[0] = "user";
 		}
 		if (isset($_POST["date"])) {
-			$données[1] = htmlspecialchars($_POST["date"]);
+			$données[1] =$_POST["date"];
 			$attr[1] = "date";
 		}
 		if (isset($_POST["title"])) {
-			$données[2] = htmlspecialchars($_POST["title"]);
+			$données[2] =$_POST["title"];
 			$attr[2] = "title";
 		}
-		if ($_POST["category"] != "noValue") {
+		if ($_POST["category"] != "all") {
 			$données[3] = $_POST["category"];
 			$attr[3] = "category";
 		}
-		
-		$server="pams.script.univ-paris-diderot.fr";
-		$user="phiear22";
-		$base="phiear22";
-		$connexion=mysql_connect($server,$user,'r1M)qu0K');
-		
 		afficherListeArticle($données, $attr, $connexion);
-		?>
-	</div>
-<?php
-}
+	}
 ?>
