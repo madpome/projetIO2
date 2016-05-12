3845b6ca9289be0c01ae10f7769334ea13f3c0d7
Philippe <earphilippefr@gmail.com>
2016-05-12 14:15:00 +0200

    Add files via upload

--- page.php
+++ page.php
@@ -1,17 +1,117 @@
 <?php
 	function welcome(){
 		if($_SESSION["connect"]){
-			echo "BONJOUR".$_SESSION["fistname"]." ".$_SESSION["lastname"]." votre mail est :".$_SESSION["mail"];
-			echo "Votre liste d'article : ";
-			echo "<div>";
-			$server="pams.script.univ-paris-diderot.fr";
-			$user="phiear22";
-			$base="phiear22";
-			$connexion=mysql_connect($server,$user,'r1M)qu0K');
-			afficherListeArticle ($_SESSION["user"], "user", $connexion);
-			echo "</div>";
+			echo "BONJOUR ".$_SESSION["firstname"]." ".$_SESSION["lastname"]." votre mail est :".$_SESSION["mail"];
 		}else{
-			echo "BONJOUR INCONNU";
+			echo 'BONJOUR INCONNU, ';
 		}
+	}
+	function footer(){
+?>
+Site crée par:
+<br>
+<table id="lespieds">
+	<tr>
+		<td>Philippe</td>
+		<td>EAR</td>
+		<td>earphilippefr@gmail.com</td>
+	</tr>
+	<tr>
+		<td>Maël</td>
+		<td>DEBOIS</td>
+		<td>maildemael@machin.com</td>
+	</tr>
+	<tr>
+		<a href=#thetop>Revenir au début</a>
+	</tr>
+</table>
+
+<?php
+	}
+	function menuvert(){
+?>
+	<div id="menuvert">
+		<ul>
+			<li>
+				<?php
+					echo "Bienvenue <br>";
+					if ($_SESSION["connect"]){
+						echo $_SESSION["lastname"]." ".$_SESSION["firstname"];
+					}else{
+						echo "invité !";
+					}
+				?>
+			</li>
+			<li>
+				<?php
+					if($_SESSION["connect"]){
+				?>
+					<a href="index.php&page=profil">Mon profil</a>
+				<?php
+					}
+				?>
+			</li>
+			<li>
+				<?php
+					if($_SESSION["connect"]){
+				?>
+					<a href="index.php&page=ecrire">Ecrire un article</a>
+				<?php
+					}
+				?>
+			</li>
+		</ul>
+	</div>
+<?php
+	}
+	function menuhori(){
+?>
+		<div id="menuhori">
+		<ul>
+			<li>
+				Articles
+				<ul>
+					<li>
+						Politiques
+					</li>
+					<li>
+						Sports
+					</li>
+					<li>
+						Chamboudi
+					</li>
+				</ul>
+			</li>
+			<li>
+				Recherche
+				<ul>
+					<li>
+						Sous1
+					</li>
+					<li>
+						Sous2
+					</li>
+					<li>
+						Sous3
+					</li>
+				</ul>
+			</li>
+			<li>
+				JSP
+				<ul>
+					<li>
+						Sous1
+					</li>
+					<li>
+						Sous2
+					</li>
+					<li>
+						Sous3
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+<?php
 	}
 ?>
