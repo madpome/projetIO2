14c0750f0d33a70c526639c47551554b3ed48822
Philippe <earphilippefr@gmail.com>
2016-05-14 14:39:12 +0200

    Update ajouterArticle.php

--- ajouterArticle.php
+++ ajouterArticle.php
@@ -2,7 +2,7 @@
 	
 	//Ajouter un article et renvoie le message correspondant au résultat de la fonction
 	function ajouterArticle ($title, $content, $category) {
-		$req = "INSERT INTO article user, title, content, date, category VALUES $_SESSION[\"user\"], \"$title\", \"$content\", NOW(), $category";
+		$req = 'INSERT INTO article user, title, content, date, category VALUES'.$_SESSION["user"].','.$title.','.$content.', NOW(),'.$category;
 		$con = mysql_connect($serv, $user);
 		if (!chercher($titre, "Titre", $con) {
 			return FALSE;
@@ -29,9 +29,9 @@
 		L'article n'a pas pu être publier, veuillez verifier que le titre n'est pas déjà utilisé.
 		<form method="post" action="ajouterArticleForm">
 			<!-- Les infos à retourner vers le formulaire d'ajout d'article - éviter la réécriture si possible. -->
-			<input type="hidden" name="title" value= <?php echo ("$_POST[\"title\"]"); ?>>
-			<input type="hidden" name="content" value=<?php echo ("$_POST[\"content\"]");?>>
-			<input type="hidden"  name="category" value=<?php echo ("$_POST[\"category\"]");?>>
+			<input type="hidden" name="title" value= <?php echo $_POST["title"]; ?>>
+			<input type="hidden" name="content" value=<?php echo $_POST["content"];?>>
+			<input type="hidden"  name="category" value=<?php echo $_POST[\"category"];?>>
 			<input type="submit" value="Retourner vers l'écriture d'article">
 		</form>
 		<a href="index.php"> Retourner à l'accueil </a>	
