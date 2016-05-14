cc70e52fec84d25f090fa3340081bda82561bebb
Philippe <earphilippefr@gmail.com>
2016-05-14 20:06:36 +0200

    Update sauvegarde.php

--- sauvegarde.php
+++ sauvegarde.php
@@ -1,5 +1,4 @@
 <?php
-include_once("verification.php");
 //enregistrement 
 function sauvegarde(){
 	$server="localhost";
@@ -10,11 +9,11 @@ function sauvegarde(){
 	if(verification()){
 		$lab=array();
 		foreach($_POST as $key=>$val){
-			$lab[$key]=mysql_real_escape_string($val);
+			$lab[$key]=@mysql_real_escape_string($val);
 		}
 		$lab["pwd"]=hachage($lab["pwd"]);
 		if($lab["pwd"]!=false){	
-			$req='INSERT INTO users (firstname,lastname,user,pwd,mail) 	VALUES ("'.mysql_real_escape_string($lab["firstname"]).'","'.mysql_real_escape_string($lab["lastname"]).'","'.mysql_real_escape_string($lab["user"]).'","'.mysql_real_escape_string($lab["pwd"]).'","'.mysql_real_escape_string($lab["mail"]).'")';
+			$req='INSERT INTO users (firstname,lastname,user,pwd,mail) 	VALUES ("'.@mysql_real_escape_string($lab["firstname"]).'","'.@mysql_real_escape_string($lab["lastname"]).'","'.@mysql_real_escape_string($lab["user"]).'","'.@mysql_real_escape_string($lab["pwd"]).'","'.@mysql_real_escape_string($lab["mail"]).'")';
 			mysql_query($req) or die("Une erreur est survenue lors de l'enregistrement:".mysql_error());
 			echo "Vous avez bien été inscrit";
 		}else{
@@ -36,7 +35,7 @@ function update(){
 	if(verificationupdate()){
 		$pwd=$_POST["pwd"];
 		$pwd=hachage($pwd);
-		$req='UPDATE users SET mail="'.mysql_real_escape_string($_POST["mail"]).'", firstname="'.mysql_real_escape_string($_POST["firstname"]).'", lastname="'.mysql_real_escape_string($_POST["lastname"]).'",pwd="'.mysql_real_escape_string($pwd).'" WHERE user="'.mysql_real_escape_string($_SESSION["user"]).'"';
+		$req='UPDATE users SET mail="'.@mysql_real_escape_string($_POST["mail"]).'", firstname="'.@mysql_real_escape_string($_POST["firstname"]).'", lastname="'.@mysql_real_escape_string($_POST["lastname"]).'",pwd="'.@mysql_real_escape_string($pwd).'" WHERE user="'.@mysql_real_escape_string($_SESSION["user"]).'"';
 		$result=mysql_query($req,$connexion);
 		getatt();
 		echo "Vos données ont bien été modifiées";
@@ -44,4 +43,3 @@ function update(){
 		echo "Vos mots de passes ne correspondent pas ou l'email est déjà utilisé";
 	}
 }
-?>
