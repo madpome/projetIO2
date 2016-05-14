ee243756352e05a8b84ff0ef785c460ab03e58f4
Philippe <earphilippefr@gmail.com>
2016-05-14 14:42:39 +0200

    Update affichage.php

--- affichage.php
+++ affichage.php
@@ -2,14 +2,14 @@
 	//Permet l'affichage d'un article entier
 	//Rajouter un lien vers la page du mec?
 	function  affiche_article($article) {
-		echo("<div><label> $article[\"title\"] </label> <label> écrit par $article[\"user\"] </label> <label> le $article[\"date\"]</div>
-		<div> $article[\"content\"] </div>
-		<div> $article[\"category\"] </div>");
+		echo  '<div>'.$article["title"].'écrit par '.$article["user"].'le '.$article["date"].'</div>';
+		echo '<div>'.$article["content"].'</div>';
+		echo '<div>'.$article["category"].'</div>';
 	}
 	
 	//Permet l'affichage partiel d'un article dans la page d'accueil
 	function affichage_partiel($article) {
-		echo("<div><label> $article[\"title\"] </label> <label> par $article[\"user\"] </label>");
+		echo  "<div>$article[\"title\"] par $article[\"user\"]");
 		echo("<img src= ");
 		switch($article["category"]) {
 			case :
