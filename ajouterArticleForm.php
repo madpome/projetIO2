803bdec4dbc339ecb5ad77191e3af5fdef3cba3f
Philippe <earphilippefr@gmail.com>
2016-05-14 14:52:34 +0200

    Update ajouterArticleForm.php

--- ajouterArticleForm.php
+++ ajouterArticleForm.php
@@ -1,43 +1,44 @@
 <?php
-	function formaddarticle(){
-		$server="pams.script.univ-paris-diderot.fr";
+//Créer un formulaire pour l'ajout d'un article
+	function formajoutarticle(){
+		$server="localhost";
 		$user="phiear22";
 		$base="phiear22";
-		$connexion=mysql_connect($server,$user,'r1M)qu0K');
-		$category = listeCategory($connexion);
-		if($_SESSION["connect"]) {
+		$connexion=@mysql_connect($server,$user,'r1M)qu0K');
+		//$category = listeCategory($connexion);
+		if($_SESSION["connect"]){
 ?>
 		<div>
 			<form method="post" action="ajouterArticle.php">
 				<?php
-				if(isset($_POST["title"]{
-					$value = $_POST["title"];
+				if(isset($_POST["title"])){
+					$value=$_POST["title"];
 				} else {
-					$value = "Votre titre";
+					$value="Votre titre (200 caractères max)";
 				}
 				?>
-				<label> Titre : <label> <input type="text" name="title" size=50 value="<?php echo $value; ?>" maxlength=200 required> <label> (200 caractères max) </label>
+				<label> Titre : <label> <input type="text" name="title" size=50 value="<?php echo $value; ?>" maxlength=200 required>
 
 				<?php
 				if(isset($_POST["content"])){
 					$value = $_POST["content"];
 				} else {
-					$value = "Votre article";
+					$value = "Votre article (20.000 caractères max)";
 				}
 				?>
-				<textarea name="content" rows="50" cols="50" maxlength="20000" required> <?php echo $value; ?> </textarea> <label> (20.000 caractères max) </label>
+				<textarea name="content" rows="50" cols="100" maxlength="20000" required> <?php echo $value; ?> </textarea>
 				
-				<select name="category">
+				<!---<select name="category">
 					<?php
 					foreach ($category as $v) {
 						echo("<option value=$v");
-						if($_POST["category"] == "$v")
+						if($_POST["category"] == "$v"){
 							echo("selected");
 						}
 						echo("> $v </option>");
 					}
 					?>
-				</select>
+				</select>--->
 				
 				<input type="submit" value="Poster cet article">
 			</form>
@@ -45,7 +46,7 @@
 			
 			
 		<?php
-		} else {
+		}else{
 		?>
 		
 			Vous devez être connecté pour poster un article
