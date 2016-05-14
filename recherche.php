1eb903e8ffafe7d1b4961475593c39b60f208203
Philippe <earphilippefr@gmail.com>
2016-05-14 19:53:04 +0200

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -1,36 +1,51 @@
 <?php
 	//Renvoie la liste des articles correspondants au(x) critère(s) contenu(s) dans $attrReq
-	function chercher($donneesReq, $attrReq, $con) {
+	function chercher($donneesReq, $attrReq) {
 		//Si les données sont dans un tableau on traite les entrées pour obtenir la requête adaptée
+		$server="localhost";
+		$user="phiear22";
+		$base="phiear22";
+		$con=@mysql_connect($server,$user,'r1M)qu0K');
+		mysql_select_db($base,$con);
 		if(is_array($donneesReq)) {
 			$req = traiter($donneesReq, $attrReq);
 		} else {
-			$req = 'SELECT * FROM article WHERE '.mysql_real_escape_string($donneesReq).'="'.mysql_real_escape_string($attrReq).'"';
+			$req = 'SELECT title,user FROM article WHERE '.@mysql_real_escape_string($attrReq).'="'.@mysql_real_escape_string($donneesReq).'"';
 		}
-		$result = mysql_query($con, $req);
-		$ret;
-		$idx = 0;
-		while ($result != false) {
-			$ret[$idx] = mysql_fetch_assoc($result);
+		$result=mysql_query($req,$con);
+		$ret=array();
+		$idx=0;
+		while ($ligne=mysql_fetch_assoc($result)) {
+			if(is_array($donneesReq)){
+				for($i=0;$i<count($attrReq);$i++){
+					$ret[$idx][$attrReq[$i]]=$ligne[$attrReq[$i]];
+				}
+			}else{
+				$ret[$idx]["user"]=$ligne["user"];
+				$ret[$idx]["title"]=$ligne["title"];
+			}
 			$idx++;
 		}
 		mysql_close();
 		return $ret;
 	}
-	
 	//Permet de taitrer les entrées $données et $attr pour la requête slq
 	//Renvoie le string de la requête slq
 	function traiter ($donneesReq, $attrReq) {
-		foreach($donneesReq as $v) {
-			$donnees = $données.$v.",";
+		$reponse='SELECT * FROM article WHERE ';
+		for($i=0;$i<count($donneesReq);$i++){
+			if($i==(count($donneesReq)-1)){
+				$reponse.=@mysql_real_escape_string($attrReq[$i]).'="'.@mysql_real_escape_string($donneesReq[$i]).'"';
+			}else{
+				$reponse.=@mysql_real_escape_string($attrReq[$i]).'="'.@mysql_real_escape_string($donneesReq[$i]).'" AND';
+			}
 		}
-		foreach($attrReq as $v) {
-			$attr = $attr.$v.",";
-		}
-		$donnees = trim($donnéesReq,",");
-		$attr = trim($attrReq,",");
-		return "SELECT * FROM article WHERE ".mysql_real_escape_string($donnees).'="'.mysql_real_escape_string($attr).'"';
+		return $reponse;
 	}
+	
+	//Permet l'affichage de la liste des articles donnée par la fonction de recherche
+	//En argument, les données pour la recherche
+	
 	
 	//Renvoie la liste des catégories dans la base de données
 	function listeCategory ($connexion) {
@@ -39,7 +54,6 @@
 			$ret[$idx] = mysql_fetch_assoc($result);
 			$idx++;
 		}
-		mysql_close();
 		return $ret;
 	}
 ?>
