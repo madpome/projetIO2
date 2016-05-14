f656e2495b1376670adc4031e02e2554ce342446
Philippe <earphilippefr@gmail.com>
2016-05-14 20:51:57 +0200

    Update formEtResultat.php

--- formEtResultat.php
+++ formEtResultat.php
@@ -47,16 +47,4 @@
 		}
 		afficherListeArticle($données, $attr);
 	}
-	
-	function affichagerecherchecategorie(){
-		foreach($categories as $v) {
-?>
-		<form action="index.php?page=resultat" method="post">
-			Catégorie <?php echo htmlentities($v);?>
-			<input type="hidden" value=<?php echo htmlentities($v);?>
-			<input type="submit" value="Afficher les articles de cette catégorie">
-		</form>
-<?php
-		}
-	}
 ?>
