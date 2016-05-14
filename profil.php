15d8ef1627442a4e5c76307272bb9ecca0f938f0
Philippe <earphilippefr@gmail.com>
2016-05-14 15:49:34 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -1,5 +1,8 @@
 <?php
     function profil(){
+        if(!isconnected()){
+            pasencoreinscrit();
+        }else{
 ?>
 <div id="profil">
     <table id="tableprofil">
@@ -28,8 +31,22 @@
             </td>
         </tr>
     </table>
-    <a href="index.php?page=inscription">Modifier mes données personnelles</a>
+    <a href="index.php?page=inscription">Modifier mes données personnelles</a><br>
+<?php
+    if($_SESSION["rank"]==1){
+        admincommande();
+    }
+?>
 </div>
+<?php
+        }
+    }
+    function pasencoreinscrit(){
+?>
+    <div id="pasencoreinscrit">
+        Vous devez être connecté pour acceder à cette page.<br>
+        <a href="index.php?page=inscription">Pas encore inscrit ? Cliquez ici !</a>
+    </div>
 <?php
     }
 ?>
