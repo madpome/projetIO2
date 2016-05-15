8d37e38090595c66336cc028564056f796f76c50
Philippe <earphilippefr@gmail.com>
2016-05-15 13:33:02 +0200

    Update formEtResultat.php

--- formEtResultat.php
+++ formEtResultat.php
@@ -2,49 +2,73 @@
 	function formrechercherarticle(){
 ?>
 	<div>
-		<form action="index.php?page=resultat" method="post">
-			 Auteur :<input type="text" name="auteur"><br>
-			 Date :<input type="date" name="date"> <br>
-			 Titre :<input type="text" name="title"><br>
-			 Rechercher dans (catégorie?):
-			 <select name="category">
-<?php
-			$categories = listeCategory();
-			foreach($categories as $v) {
-				echo '<option value='.$v.'>'.$v.'</option>';
-			}
-?>
-				<option value="all"> Toutes catégories </option>
-			</select>
-			<input type="submit" value="Lancer la recherche">
-		</form>
+		<table>
+			<form action="index.php?page=resultat" method="post">
+			<tr>
+				<td>
+				Auteur :
+				</td>
+				<td>
+				<input type="text" name="auteur">
+				</td>
+			</tr>
+			<tr>
+				<td>
+				Titre :
+				</td>
+				<td>
+				<input type="text" name="title">
+				</td>
+			</tr>
+			<tr>
+				<td>
+				Catégorie:
+				</td>
+				<td>
+				<select name="category">
+	<?php
+				$categories = listeCategory();
+				foreach($categories as $v) {
+					echo '<option value='.$v.'>'.$v.'</option>';
+				}
+	?>
+					<option value="all"> Toutes catégories </option>
+				</select>
+				</td>
+				<td>
+			</tr>
+			<tr>
+				<td>
+				</td>
+				<td>
+				<input type="submit" value="Lancer la recherche">
+				</td>
+			</tr>
+			</form>
+		</table>
 	</div>
+	
 <?php
 	}
 	function resultatrecherche(){
-		$données;
-		$attr;
+		$donnees=array();
+		$attr=array();
 		$idx=0;
-		if (!empty($_POST["auteur"])) {
-			$données[$idx] =$_POST["auteur"];
+		if(!empty($_POST["auteur"])) {
+			$donnees[$idx] =$_POST["auteur"];
 			$attr[$idx] = "user";
 			$idx++;
 		}
-		if (!empty($_POST["date"])) {
-			$données[$idx] =$_POST["date"];
-			$attr[$idx] = "date";
+		if(!empty($_POST["title"])) {
+			$donnees[$idx] =$_POST["title"];
+			$attr[$idx]="title";
 			$idx++;		
 		}
-		if (!empty($_POST["title"])) {
-			$données[$idx] =$_POST["title"];
-			$attr[$idx] = "title";
-			$idx++;		
-		}
-		if ($_POST["category"] != "all") {
-			$données[$idx] = $_POST["category"];
-			$attr[$idx] = "category";
+		if($_POST["category"]!="all") {
+			$donnees[$idx]=$_POST["category"];
+			$attr[$idx]="category";
 			$idx++;
 		}
-		afficherListeArticle($données, $attr);
+		afficherListeArticle($donnees,$attr);
 	}
 ?>
